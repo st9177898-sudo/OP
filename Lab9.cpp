795de6fbf8434aa8fdd7c8dbaa98d3624b853390
Oleg Petrenko <st9177898@stud.duikt.edu.ua>
2025-12-22 00:03:27 +0200

    Refactor Lab9.cpp for improved readability

--- Lab9.cpp
+++ Lab9.cpp
@@ -42,15 +42,13 @@ int main() {
     Airport kyiv("Бориспіль", "KBP");
     Airport lax("Лос-Анджелес", "LAX");
     Airport sfo("Сан-Франциско", "SFO");
-
     Airplane plane1("Boeing 737", 180);
     Airplane plane2("Airbus A320", 160);
-
     plane1.addRoute(new Route(kyiv, lax, 3983));
     plane1.addRoute(new Route(lax, sfo, 543));
     plane2.addRoute(new Route(sfo, kyiv, 4130));
-
     plane1.printInfo();
     cout << endl;
     plane2.printInfo();
+
 }
